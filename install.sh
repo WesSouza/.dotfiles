d2eccbb5c9c241c001be34ff93e52ecfc3b6959f
Wes Souza <wesleywex@gmail.com>
2019-01-16 16:00:03 -0500

    Add .gitignore

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ cd `dirname $0`
 DIRNAME=`pwd`
 
 ln -s $DIRNAME/.bash_aliases ~/.bash_aliases
+ln -s $DIRNAME/.gitignore ~/.gitignore
 
 echo "# Added automatically" >> ~/.bash_profile
 echo "source $DIRNAME/.bash_profile" >> ~/.bash_profile
+
+git config --global core.excludesfile ~/.gitignore
